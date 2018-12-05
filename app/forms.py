08c9d3637d2b6ef6da4a112f9720527c2a2a8674
Sean Murphy <mail.seanmurphy@gmail.com>
2018-12-05 17:17:47 +0000

    Added form validation to form.py

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,19 @@ from wtforms.validators import DataRequired, ValidationError
 
 
 
-# LoginForm
+# Login / Register Form
 class LoginForm(FlaskForm):
+    # Fields
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('submit')
 
+
     def validate_username(self, username):
-        if len(username) < 4:
+        if len(username.data) < 4:
+            print('Hello')
             raise ValidationError('username must be longer than 4 characters')
 
     def validate_password(self, password):
-        if len(password) < 4:
+        if len(password.data) < 4:
             raise ValidationError('password must be longer than 4 characters')
--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,10 @@ def index():
     # new instance of LoginForm
     login_form = LoginForm()
 
+    #
+    if login_form.validate_on_submit():
+        pass
+
     # default - rentrun index.html
     return render_template('index.html', login_form=login_form)
 
