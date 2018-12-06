b396ae64dcb69dc2d7e7da8794fa4212b00c8b47
Sean Murphy <mail.seanmurphy@gmail.com>
2018-12-06 14:54:36 +0000

    created dummy leaderboard scores in data/leaderboard.json

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import LoginForm
 
 
@@ -14,12 +14,30 @@ def index():
     # new instance of LoginForm
     login_form = LoginForm()
 
-    #
+    # login in user
     if login_form.validate_on_submit():
         pass
 
-    # default - rentrun index.html
+    # default - render index.html
     return render_template('index.html', login_form=login_form)
+
+
+#=====================#
+
+
+# leaderboard
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')
+
+
+#=====================#
+
+
+# logout
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
 
 
 #=====================#
