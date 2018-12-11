7b05402d6843de2a0bd308759d1756597065624f
Sean Murphy <mail.seanmurphy@gmail.com>
2018-12-11 21:09:18 +0000

    updated run to include os.eviron.get IP & PORT

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import app
 
 # run app and set debug on
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
