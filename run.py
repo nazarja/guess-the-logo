1f4735beb15d1bf57e43cdd9113c2b7ba7c59238
Sean Murphy <mail.seanmurphy@gmail.com>
2018-12-12 12:20:22 +0000

    set app.run for heroku

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import app
 
 # run app and set debug on
 if __name__ == '__main__':
-    # app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'))
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'))
+    # app.run(debug=True)
